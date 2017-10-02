18aa0b789b1c127142d3511645237468ccd4547a
mANIApHOBIC <git@maniaphobic.org>
2017-10-02 15:09:58 -0700

    I added a :gem_name resource property
    This commit augments the "_mariadb" and "_mysql" resources with a
    property named "gem_name", whose default is "mysql2". This property
    allows one to specify an alternate gem name in situations where
    "mysql2" cannot be used. It applies in particular to use cases where
    the deployment environment lacks a C compiler and one must pre-compile
    binary extensions into a gem with another name.
    
    Change-Id: I93ffb817fc243ac0243625a9d1d2e7cbd69fee5d

--- resources/mysql2_chef_gem_mysql.rb
+++ resources/mysql2_chef_gem_mysql.rb
@@ -1,9 +1,12 @@
 property :mysql2_chef_gem_name, String, name_property: true, required: true
+property :gem_name, String, default: 'mysql2'
 property :gem_version, String, default: '0.4.9'
 property :package_version, String
 
 provides :mysql2_chef_gem
 provides :mysql2_chef_gem_mysql
+
+gem_binary_path = RbConfig::CONFIG['bindir'] + '/gem'
 
 action :install do
   include_recipe 'build-essential::default'
@@ -14,16 +17,20 @@ action :install do
     action :create
   end
 
-  gem_package 'mysql2' do
-    gem_binary RbConfig::CONFIG['bindir'] + '/gem'
+  gem_name = new_resource.gem_name
+  gem_package "install_gem_package_#{gem_name}" do
+    gem_binary gem_binary_path
+    package_name gem_name
     version new_resource.gem_version
     action :install
   end
 end
 
 action :remove do
-  gem_package 'mysql2' do
-    gem_binary RbConfig::CONFIG['bindir'] + '/gem'
+  gem_name = new_resource.gem_name
+  gem_package "remove_gem_package_#{gem_name}" do
+    gem_binary gem_binary_path
+    package_name gem_name
     action :remove
   end
 end
